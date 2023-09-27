fae186fe2dc0e89e2442d992f9e347fe0778405d
joaopamarall <joaoamaral56@hotmail.com>
2023-09-27 15:54:21 -0300

    First commit

---
+++ components/Chips.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="chip" :class="{ 'active': isActive }" @click="handleClick">
+    {{ gender }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chips',
+  props: {
+    gender: String,
+    isActive: Boolean,
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click', this.gender);
+    },
+  },
+};
+</script>
+
+<style>
+.chip {
+  background-color: rgb(206, 230, 241);
+  color: rgb(90, 136, 121);
+  border: 2px solid rgb(90, 136, 121);
+  border-radius: 40px;
+  padding: 8px;
+  cursor: pointer;
+}
+
+.chip.active {
+  background-color: rgba(93, 122, 137, 1);
+  color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="chips">
+    <div v-for="(gender, index) in genderOptions" :key="index">
+      <Chips :gender="gender" :active="gender === selectedGender" @click.native="selectGender(gender)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Chips from '@/components/Chips.vue';
+
+export default {
+  name: "indexPage",
+  components: {
+    Chips,
+  },
+  data() {
+    return {
+      genderOptions: ["Mulheres", "Homens", "Trans"],
+      selectedGender: "Mulheres",
+    };
+  },
+  methods: {
+    selectGender(gender) {
+      this.selectedGender = gender;
+      const newRoute = `/gender/${gender.toLowerCase()}`;
+      window.location.pathname = newRoute;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: rgb(206, 230, 241);
+}
+
+.chips {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+</style>
